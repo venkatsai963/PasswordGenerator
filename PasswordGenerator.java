7bf7014364169226e8c89499d4ef66f9f9e64f72
venkatsai963 <venkatgunji298@gmail.com>
2025-01-30 23:57:14 +0530

    Add files via upload

---
+++ PasswordGenerator.java
@@ -0,0 +1,43 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_+=<>?";
+    private static final String ALL_CHARACTERS = UPPERCASE + LOWERCASE + DIGITS + SPECIAL_CHARACTERS;
+
+    public static void main(String[] args) {
+        int passwordLength = 12; // Desired password length
+        String password = generatePassword(passwordLength);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        if (length < 4) {
+            throw new IllegalArgumentException("Password length must be at least 4 to include all character types.");
+        }
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+        password.append(LOWERCASE.charAt(random.nextInt(LOWERCASE.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        password.append(SPECIAL_CHARACTERS.charAt(random.nextInt(SPECIAL_CHARACTERS.length())));
+
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARACTERS.charAt(random.nextInt(ALL_CHARACTERS.length())));
+        }
+
+        char[] passwordArray = password.toString().toCharArray();
+        for (int i = passwordArray.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = passwordArray[i];
+            passwordArray[i] = passwordArray[j];
+            passwordArray[j] = temp;
+        }
+
+        return new String(passwordArray);
+}
+}
